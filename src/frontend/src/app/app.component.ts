9d29cceaa96f5c77b02d052635e4db284e5912bf
mrtwisniewska <marta.wisniewska116@gmail.com>
2024-01-13 19:36:19 +0100

    Merged with develop, added font styles, added footer template

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
-import { HowToComponent } from "./pages/how-to/how-to.component";
+import { FooterComponent } from "./pages/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, NavBarComponent, HowToComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'fit-wallet';
--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HowToComponent } from './pages/how-to/how-to.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'how-to', pathMatch: 'full' },
   { path: 'how-to', component: HowToComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'contact', component: ContactComponent},
 ];
