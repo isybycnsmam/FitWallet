9def2de4bcb925413b8eaf9c5b4caaa9c326ae6b
mrtwisniewska <marta.wisniewska116@gmail.com>
2024-01-13 22:46:42 +0100

    Added completed how-to page, more icons. Fixed problems with footer width

--- src/frontend/src/app/pages/how-to/how-to.component.ts
+++ src/frontend/src/app/pages/how-to/how-to.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-how-to',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './how-to.component.html',
   styleUrl: './how-to.component.scss'
 })
