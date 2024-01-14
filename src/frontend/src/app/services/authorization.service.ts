cd18bf1afce2e694449ef48015d472872f6f3f51
Maciej Scholz <solz@viessmann.com>
2024-01-14 20:32:02 +0100

    Fixed login and register actions

--- src/frontend/src/app/services/authorization.service.ts
+++ src/frontend/src/app/services/authorization.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorizationService {
+  tokenModifiedSubject = new BehaviorSubject<boolean>(false);
+
   saveToken(token: { accessToken: string }) {
     localStorage.setItem('jwt_token', token.accessToken);
+    this.tokenModifiedSubject.next(this.isLoggedIn());
   }
 
   getToken() {
@@ -14,6 +18,7 @@ export class AuthorizationService {
 
   removeToken() {
     localStorage.removeItem('jwt_token');
+    this.tokenModifiedSubject.next(this.isLoggedIn());
   }
 
   isLoggedIn() {
